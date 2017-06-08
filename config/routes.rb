c55a75be2aeaadb8a26caa22c74541438fa1ffeb
ChristopherZev <christopher_zev@icloud.com>
2017-06-08 05:59:59 -0400

    Setup application HomePage by modifying the config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  root 'welcome#index' #tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action. This was created earlier when I ran the controller generator (bin/rails generate controller Welcome index)
 
+    
+    
+    
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
